090c1b5f5c2ae3a7ee4547224ad78b4760bd56f0
Cameron Viner <cameron.viner@gmail.com>
2025-05-07 20:26:22 +0100

    Upgrade from tailwind v3 to tailwind v4

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,15 @@
 import { defineConfig } from "astro/config";
 import netlify from "@astrojs/netlify";
 import react from "@astrojs/react";
-
-import tailwind from "@astrojs/tailwind";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   adapter: netlify(),
-  integrations: [react(), tailwind({ applyBaseStyles: false })],
+  integrations: [react()],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
